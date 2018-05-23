cc05feb142018946dae280a96609faf116a24f3d
Denis Simagin <dasimagin@yandex-team.ru>
2018-05-23 19:29:20 +0300

    Add example to make one executable

---
+++ src/example.py
@@ -0,0 +1,11 @@
+import numpy
+import scipy
+import tensorflow
+
+# For fucking keras wankers
+import scipy._lib.messagestream
+import keras
+
+
+with open('destination_example.txt') as lines:
+    print(lines.read())
